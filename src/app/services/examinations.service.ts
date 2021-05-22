e6dba284d1c1a7c6984d181452b6efef8c869712
Aleksey Semenov <43957407+borgishmorg@users.noreply.github.com>
2021-05-22 22:27:54 +0200

    feat: add examinations service

---
+++ src/app/services/examinations.service.ts
@@ -0,0 +1,70 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '@enviroment';
+import { Moment } from 'moment';
+import { Patient } from './patients.service';
+import { User } from './user.service';
+
+export interface ExaminationPost {
+  complaints: string;
+  anamnesis: string;
+  diagnosis: string;
+  recomendations: string;
+  user_id: number;
+  patient_id: number;
+}
+
+export interface Examination extends ExaminationPost {
+  id: number;
+  datetime: Moment;
+  user: User;
+  patient: Patient;
+}
+
+export interface Examinations {
+  total: number;
+  examinations: Examination[];
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExaminationsService {
+  constructor(private http: HttpClient) {}
+
+  search(patient_id: number) {
+    return this.http.get<Examinations>(`${environment.apiUrl}/examinations`, {
+      params: {
+        patient_id: patient_id.toString(),
+      },
+    });
+  }
+
+  getAll() {
+    return this.http.get<Examinations>(`${environment.apiUrl}/examinations`);
+  }
+
+  get(id: number) {
+    return this.http.get<Examination>(
+      `${environment.apiUrl}/examinations/${id}`
+    );
+  }
+
+  post(examination: ExaminationPost) {
+    return this.http.post<Examination>(
+      `${environment.apiUrl}/examinations`,
+      examination
+    );
+  }
+
+  put(id: number, examination: ExaminationPost) {
+    return this.http.put<Examination>(
+      `${environment.apiUrl}/examinations/${id}`,
+      examination
+    );
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${environment.apiUrl}/examinations/${id}`);
+  }
+}

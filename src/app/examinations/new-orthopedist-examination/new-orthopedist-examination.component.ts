ee8165edffcc32b460f8d6e9a2887e0c44d65c1b
Aleksey Semenov <43957407+borgishmorg@users.noreply.github.com>
2021-05-27 21:43:54 +0200

    feat: add new orthopedist examination component

--- src/app/examinations/new-orthopedist-examination/new-orthopedist-examination.component.ts
+++ src/app/examinations/new-orthopedist-examination/new-orthopedist-examination.component.ts
@@ -1,15 +1,131 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PermissionEnum } from '@app/auth.guard';
+import { AuthenticationService } from '@app/services/authentication.service';
+import {
+  ExaminationsService,
+  ExaminationType,
+  OrthopedistExaminationPost,
+} from '@app/services/examinations.service';
+import { LoadingService } from '@app/services/loading.service';
+import { NotificationsService } from '@app/services/notifications.service';
+import { Patient, PatientsService } from '@app/services/patients.service';
+import { User, UsersService } from '@app/services/user.service';
+import { throwError } from 'rxjs';
+import { catchError, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-orthopedist-examination',
   templateUrl: './new-orthopedist-examination.component.html',
-  styleUrls: ['./new-orthopedist-examination.component.css']
+  styleUrls: ['./new-orthopedist-examination.component.css'],
 })
 export class NewOrthopedistExaminationComponent implements OnInit {
+  examination?: OrthopedistExaminationPost;
+  oldExamination?: OrthopedistExaminationPost;
 
-  constructor() { }
+  patient?: Patient;
+  user?: User;
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthenticationService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private loadingService: LoadingService,
+    private notificationsService: NotificationsService,
+    private examinationsService: ExaminationsService,
+    private patientsService: PatientsService,
+    private usersService: UsersService
+  ) {}
+
+  get canView() {
+    return this.authService.hasPemissions([
+      PermissionEnum.ORTHOPEDIST_EXAMINATIONS_VIEW,
+    ]);
   }
 
+  get canEdit() {
+    return this.authService.hasPemission(
+      PermissionEnum.ORTHOPEDIST_EXAMINATIONS_EDIT
+    );
+  }
+
+  ngOnInit(): void {
+    if (this.authService.userValue) {
+      this.loadingService.startLoading();
+      const user_id = this.authService.userValue.id;
+
+      this.usersService
+        .get(user_id)
+        .pipe(
+          mergeMap((user) => {
+            this.user = user;
+            return this.route.paramMap;
+          })
+        )
+        .pipe(
+          mergeMap((params) => {
+            const patient_id = params.get('patient_id');
+            if (!patient_id || !+patient_id) return throwError('');
+            return this.patientsService.get(+patient_id);
+          })
+        )
+        .pipe(
+          catchError((error) => {
+            this.router.navigate(['notfound']);
+            return throwError(error);
+          })
+        )
+        .subscribe((patient) => {
+          this.patient = patient;
+          this.examination = {
+            anamnesis: '',
+            complaints: '',
+            objectively: '',
+            diagnosis: '',
+            recomendations: '',
+            type: ExaminationType.ORTHOPEDIST,
+            asymmetry: '',
+            foot_deformation: '',
+            lower_limb_axis: '',
+            lower_limb_joints_functions: '',
+            neurovascular_disorders: '',
+            spine_axis: '',
+            upper_limb_axis: '',
+            upper_limb_joints_functions: '',
+            user_id: user_id,
+            patient_id: patient.id,
+          };
+          this.oldExamination = JSON.parse(JSON.stringify(this.examination));
+          this.loadingService.stopLoading();
+        });
+    }
+  }
+
+  back() {
+    this.router.navigate([`/patients/${this.examination?.patient_id}`]);
+  }
+
+  discard() {
+    this.examination = JSON.parse(JSON.stringify(this.oldExamination));
+  }
+
+  save() {
+    if (this.examination) {
+      this.loadingService.startLoading();
+      this.examinationsService
+        .post(this.examination)
+        .pipe(
+          catchError((error) => {
+            this.notificationsService.error(error);
+            this.loadingService.stopLoading();
+            return throwError(error);
+          })
+        )
+        .subscribe((examination) => {
+          this.router.navigate([
+            `/patients/${examination.patient_id}/examinations/${examination.id}`,
+          ]);
+        });
+    }
+  }
 }
